f31d7e349ea259e8e37b8031efdf3138980a1660
guisela29 <g.roxana.martinez@gmail.com>
2025-09-15 20:48:00 -0500

    Initial commit

---
+++ src/main/java/org/softprimesolutions/carritoapp/CarritoAppApplication.java
@@ -0,0 +1,13 @@
+package org.softprimesolutions.carritoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/softprimesolutions/carritoapp/CarritoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softprimesolutions.carritoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
